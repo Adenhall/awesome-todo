e132b536b131f1f24a67257d30f8d22212c09681
Adenhall <jamesadenhall@gmail.com>
2024-07-23 18:09:10 +0700

    feat(rails): add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.create([
+              { title: 'Call references', subtitle: 'Job: ABC123 - UX Designer', priority: :high,
+                due_date: Date.today },
+              { title: 'Conduct police check & send emails', subtitle: 'Application: 12803849 - Calliope Maddison',
+                priority: :medium, due_date: 1.day.from_now },
+              { title: 'Ask candidates about driver licenses and blue card',
+                subtitle: 'Job: ABC321 - Aboriginal Family Partnership Worker', priority: :low, due_date: 2.days.from_now }
+            ])
