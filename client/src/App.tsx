3c21d792a6bc7975b4896d701ed6f4680370bc64
Adenhall <jamesadenhall@gmail.com>
2024-07-23 15:30:01 +0700

    feat(client): install tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       await fetch("/api/health_check");
     })();
   }, []);
-  return <h1>Hello world</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world</h1>;
 }
 
 export default App;
