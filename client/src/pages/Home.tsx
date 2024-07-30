092f80c210f3d5b6aa62866253779ecc7412fe34
Adenhall <jamesadenhall@gmail.com>
2024-07-30 21:12:31 +0700

    feat: install drag and drop

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { getIncompleteTasks, Task as ApiTask } from "../services/tasks";
 
 import Task from "../components/Task";
 import { LoaderFunction, useLoaderData, useNavigate } from "react-router-dom";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
 
 export const loader: LoaderFunction = async () => {
   return getIncompleteTasks();
@@ -18,19 +19,41 @@ const Home = () => {
         <h1 className="text-3xl font-bold p-6">Tasks</h1>
         <button onClick={() => navigate("/create")}>Create new todo</button>
       </div>
-      <div className="p-6">
-        {tasks.map(({ id, title, subtitle, due_date, priority }) => (
-          <Task
-            key={id}
-            id={id}
-            title={title}
-            subtitle={subtitle}
-            priority={priority}
-            dueDate={due_date}
-            onChecked={(id) => setTasks(tasks.filter((task) => task.id !== id))}
-          />
-        ))}
-      </div>
+      <DragDropContext onDragEnd={() => console.log("dropped")}>
+        <Droppable droppableId="tasks-drop">
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="p-6"
+            >
+              {tasks.map(({ id, title, subtitle, due_date, priority }, idx) => (
+                <Draggable draggableId={id} index={idx}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <Task
+                        key={id}
+                        id={id}
+                        title={title}
+                        subtitle={subtitle}
+                        priority={priority}
+                        dueDate={due_date}
+                        onChecked={(id) =>
+                          setTasks(tasks.filter((task) => task.id !== id))}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
