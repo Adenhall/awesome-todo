437f671a00c51ba366e199acc4898d59626f0754
Adenhall <jamesadenhall@gmail.com>
2024-07-30 22:16:39 +0700

    feat: reordering on drop

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { getIncompleteTasks, Task as ApiTask } from "../services/tasks";
 
-import Task from "../components/Task";
 import { LoaderFunction, useLoaderData, useNavigate } from "react-router-dom";
-import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import { DragDropContext, Draggable, Droppable, OnDragEndResponder } from "@hello-pangea/dnd";
+
+import { getIncompleteTasks, Task as ApiTask } from "../services/tasks";
+import Task from "../components/Task";
 
 export const loader: LoaderFunction = async () => {
   return getIncompleteTasks();
@@ -13,13 +14,24 @@ const Home = () => {
   const { data } = useLoaderData() as { data: ApiTask[] };
   const [tasks, setTasks] = useState<ApiTask[]>(data);
   const navigate = useNavigate();
+
+  const handleDrop: OnDragEndResponder = (result) => {
+    const { destination, source } = result
+    if (!destination) return;
+    setTasks((tasks) => {
+      tasks.splice(destination.index, 0, tasks.splice(source.index, 1)[0]);
+
+      return [...tasks];
+    })
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col p-6 divide-y">
       <div className="flex items-center gap-6">
         <h1 className="text-3xl font-bold p-6">Tasks</h1>
         <button onClick={() => navigate("/create")}>Create new todo</button>
       </div>
-      <DragDropContext onDragEnd={() => console.log("dropped")}>
+      <DragDropContext onDragEnd={handleDrop}>
         <Droppable droppableId="tasks-drop">
           {(provided) => (
             <div
@@ -28,7 +40,7 @@ const Home = () => {
               className="p-6"
             >
               {tasks.map(({ id, title, subtitle, due_date, priority }, idx) => (
-                <Draggable draggableId={id} index={idx}>
+                <Draggable key={id} draggableId={id} index={idx}>
                   {(provided) => (
                     <div
                       ref={provided.innerRef}
@@ -36,7 +48,6 @@ const Home = () => {
                       {...provided.dragHandleProps}
                     >
                       <Task
-                        key={id}
                         id={id}
                         title={title}
                         subtitle={subtitle}
